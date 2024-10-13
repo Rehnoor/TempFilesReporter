cdd0a6cf4179bbcda01d6ffedfff5349e9db41cb
Rehnoor <rehnoorsaini@outlook.com>
2024-10-12 22:34:09 -0700

    Now script.py collects info on last modified and size of directories and files

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
-from datetime import datetime
-# import PySimpleGUI as psg
+import script
 import tkinter as tk
 
 window = tk.Tk()
 window.title("Temp File Reporter")
 window.geometry("300x300")
+
+print(script.tempFiles)
 
 
 window.mainloop()
--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 import os
+import pathlib as Path
+from datetime import datetime
 
 tempFiles = []
 tempFolders = []
 
-
+testDir = os.getcwd()
 
 def isTempFile(extName):
     return (extName == ".cache") or (extName == ".tmp") or (extName == ".log") or (extName == ".part") or (extName == ".partial")
@@ -11,6 +13,22 @@ def isTempFile(extName):
 
 def isTempFolder(folderName):
     return (folderName == "tmp") or (folderName == ".cache")
+
+def stringifyBytes(bytes):
+    kb = bytes/1000
+    if (kb < 1):
+        return str(bytes) + " Bytes"
+    mb = kb/1000
+    if (mb < 1):
+        return str(kb) + " KB"
+    gb = mb/1000
+    if (gb < 1):
+        return str(mb) + " MB"
+    tb = gb/1000
+    if (tb < 1):
+        return str(gb) + " GB"
+    return str(tb) + " TB"
+    
 
 
 for dirpath, dirnames, filenames in os.walk('/Users/sainir/repos'):
@@ -21,26 +39,36 @@ for dirpath, dirnames, filenames in os.walk('/Users/sainir/repos'):
             # print("Directories: ", dirnames)
             # print("Files: ", filenames)
             # print()
-            tup = (dirpath, fName)
+            fullFilePath = os.path.join(dirpath, fName)
+            fileStats = os.stat(fullFilePath)
+            fileSize = stringifyBytes(fileStats.st_size)
+            modTime = datetime.fromtimestamp(fileStats.st_mtime)
+            tup = (dirpath, fName, fileSize, modTime)
             tempFiles.append(tup)
     for dName in dirnames:
         if (isTempFolder(dName)):
-            tup = (dirpath, dName)
+            fullDirPath = os.path.join(dirpath, dName)
+            dirStats = os.stat(fullDirPath)
+            dirSize = stringifyBytes(dirStats.st_size)
+            modTime = datetime.fromtimestamp(dirStats.st_mtime)
+            tup = (dirpath, dName, dirSize, modTime)
             tempFolders.append(tup)
 
 
-print("HERE AT THE TEMPORARY FILES AT /Users/sainir/repos")
+print("HERE ARE THE TEMPORARY FILES AT /Users/sainir/repos")
 print("----------------------------------------------------")
 
 for tup in tempFiles:
-    dirpath, fName = tup
-    print("There is a temporary file called " + fName + " at the directory " + dirpath)
+    dirpath, fName, fileSize, modTime = tup
+    print("There is a temporary file called " + fName + " at the directory " + dirpath + ".")
+    print("-Size: " + fileSize + ", Last Modified: " + str(modTime))
     print()
 
 print("HERE ARE THE TEMPORARY FOLDERS AT /Users/sainir/repos")
 print("-------------------------------------------------------")
 for tup in tempFolders:
-    dirpath, dName = tup
+    dirpath, dName, fileSize, modTime = tup
     print("There is a temporary folder called " + dName + " at the directory " + dirpath)
+    print("-Size: " + fileSize + ", Last Modified: " + str(modTime))
     print()
     
