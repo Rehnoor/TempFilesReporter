8f21cea5d1428a07ced4265ad4110deca31f380b
Rehnoor <rehnoorsaini@outlook.com>
2024-10-13 22:16:09 -0700

    Created initial GUI, can not display files on GUI yet

--- main.py
+++ main.py
@@ -1,13 +1,83 @@
 import os
 import script
+import testinit
 import tkinter as tk
+from tkinter import messagebox
 
 window = tk.Tk()
 window.title("Temp File Reporter")
-window.geometry("300x300")
+window.geometry("600x600")
 
-print(script.tempFiles)
+# Grid configuration
+window.columnconfigure(0, weight=1)  # Makes sure the window scales properly
+
+# Description label
+descLab = tk.Label(window, text="Please enter below which directory you would like to be scanned for temporary files:")
+descLab.grid(row=0, column=0, padx=10, pady=10)
+
+# Directory label
+dirLab = tk.Label(window, text="Directory:")
+dirLab.grid(row=1, column=0, padx=10, pady=10, sticky='w')
+
+# Directory entry
+dirEntry = tk.Entry(window)
+dirEntry.grid(row=2, column=0, padx=10, pady=10, sticky='ew')
+
+# Table Header
+tblHdr = tk.Label(window, text="")
+
+def stringifyBytes(bytes):
+    kb = bytes/1000
+    if (kb < 1):
+        return str(bytes) + " Bytes"
+    mb = kb/1000
+    if (mb < 1):
+        return str(kb) + " KB"
+    gb = mb/1000
+    if (gb < 1):
+        return str(mb) + " MB"
+    tb = gb/1000
+    if (tb < 1):
+        return str(gb) + " GB"
+    return str(tb) + " TB"
+
+# function for processing submission
+def dir_submit():
+    enteredDir = dirEntry.get()
+    if not os.path.exists(enteredDir):
+        messagebox.showerror("Invalid directory", "You entered an invalid directory! Please enter a valid one")
+    else:
+        script.getTemp(enteredDir)
+        printLog()
+        startingRow = 4
+        currRow = startingRow
+
+
+
+
+dirSub = tk.Button(window, text="Submit", command=dir_submit)
+dirSub.grid(row=3, column=0, padx=10, pady=10)
+
+
+
+
+def printLog():
+    print("HERE ARE THE TEMPORARY FILES")
+    print("----------------------------------------------------")
+
+    for tup in script.tempFiles:
+        dirpath, fName, fileSize, modTime = tup
+        print("There is a temporary file called " + fName + " at the directory " + dirpath + ".")
+        print("-Size: " + str(fileSize) + ", Last Modified: " + str(modTime))
+        print()
+
+    print("HERE ARE THE TEMPORARY FOLDERS")
+    print("-------------------------------------------------------")
+    for tup in script.tempFolders:
+        dirpath, dName, fileSize, modTime = tup
+        print("There is a temporary folder called " + dName + " at the directory " + dirpath)
+        print("-Size: " + str(fileSize) + ", Last Modified: " + str(modTime))
+        print()
 
 
 window.mainloop()
-# psg.popup("Hello World")
--- script.py
+++ script.py
@@ -14,61 +14,29 @@ def isTempFile(extName):
 def isTempFolder(folderName):
     return (folderName == "tmp") or (folderName == ".cache")
 
-def stringifyBytes(bytes):
-    kb = bytes/1000
-    if (kb < 1):
-        return str(bytes) + " Bytes"
-    mb = kb/1000
-    if (mb < 1):
-        return str(kb) + " KB"
-    gb = mb/1000
-    if (gb < 1):
-        return str(mb) + " MB"
-    tb = gb/1000
-    if (tb < 1):
-        return str(gb) + " GB"
-    return str(tb) + " TB"
     
 
-
-for dirpath, dirnames, filenames in os.walk('/Users/sainir/repos'):
-    for fName in filenames:
-        # print(os.path.splitext(fNames))
-        if (isTempFile(os.path.splitext(fName)[1])):
-            # print("Current path: ", dirpath)
-            # print("Directories: ", dirnames)
-            # print("Files: ", filenames)
-            # print()
-            fullFilePath = os.path.join(dirpath, fName)
-            fileStats = os.stat(fullFilePath)
-            fileSize = stringifyBytes(fileStats.st_size)
-            modTime = datetime.fromtimestamp(fileStats.st_mtime)
-            tup = (dirpath, fName, fileSize, modTime)
-            tempFiles.append(tup)
-    for dName in dirnames:
-        if (isTempFolder(dName)):
-            fullDirPath = os.path.join(dirpath, dName)
-            dirStats = os.stat(fullDirPath)
-            dirSize = stringifyBytes(dirStats.st_size)
-            modTime = datetime.fromtimestamp(dirStats.st_mtime)
-            tup = (dirpath, dName, dirSize, modTime)
-            tempFolders.append(tup)
-
-
-print("HERE ARE THE TEMPORARY FILES AT /Users/sainir/repos")
-print("----------------------------------------------------")
-
-for tup in tempFiles:
-    dirpath, fName, fileSize, modTime = tup
-    print("There is a temporary file called " + fName + " at the directory " + dirpath + ".")
-    print("-Size: " + fileSize + ", Last Modified: " + str(modTime))
-    print()
-
-print("HERE ARE THE TEMPORARY FOLDERS AT /Users/sainir/repos")
-print("-------------------------------------------------------")
-for tup in tempFolders:
-    dirpath, dName, fileSize, modTime = tup
-    print("There is a temporary folder called " + dName + " at the directory " + dirpath)
-    print("-Size: " + fileSize + ", Last Modified: " + str(modTime))
-    print()
+def getTemp(dir):
+    for dirpath, dirnames, filenames in os.walk(dir):
+        for fName in filenames:
+            # print(os.path.splitext(fNames))
+            if (isTempFile(os.path.splitext(fName)[1])):
+                # print("Current path: ", dirpath)
+                # print("Directories: ", dirnames)
+                # print("Files: ", filenames)
+                # print()
+                fullFilePath = os.path.join(dirpath, fName)
+                fileStats = os.stat(fullFilePath)
+                fileSize = fileStats.st_size
+                modTime = datetime.fromtimestamp(fileStats.st_mtime)
+                tup = (dirpath, fName, fileSize, modTime)
+                tempFiles.append(tup)
+        for dName in dirnames:
+            if (isTempFolder(dName)):
+                fullDirPath = os.path.join(dirpath, dName)
+                dirStats = os.stat(fullDirPath)
+                dirSize = dirStats.st_size
+                modTime = datetime.fromtimestamp(dirStats.st_mtime)
+                tup = (dirpath, dName, dirSize, modTime)
+                tempFolders.append(tup)
     
